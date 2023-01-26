5b12ff709cce4c354c210623588c291fe1e6782f
TIEW CHUAN YONG <2102030J@student.tp.edu.sg>
2023-01-26 11:03:56 +0800

    Test

---
+++ Test/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Test.Server.Data;
+using Test.Server.Models;
+
+[assembly: HostingStartup(typeof(Test.Server.Areas.Identity.IdentityHostingStartup))]
+namespace Test.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
