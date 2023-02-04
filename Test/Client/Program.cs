ee1b3914724ef1ed3e806b5ff82a4407e1b0587a
TIEW CHUAN YONG <2102030J@student.tp.edu.sg>
2023-02-04 11:53:17 +0800

    Week 13 Added HttpInterceptorService Added 404, 500, unauthorize

--- Test/Client/Program.cs
+++ Test/Client/Program.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Test.Client.Services;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace Test.Client
 {
@@ -18,11 +20,16 @@ namespace Test.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("Test.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            builder.Services.AddHttpClient("CarRentalManagement.ServerAPI", (sp,client) => {client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                   client.EnableIntercept(sp);})
+                    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Test.ServerAPI"));
+
+            builder.Services.AddHttpClientInterceptor();
+
+            builder.Services.AddScoped<HttpInterceptorService>();
 
             builder.Services.AddApiAuthorization();
 
